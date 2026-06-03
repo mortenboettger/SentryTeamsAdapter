fd60d0030abddf47e9d95941abd04f0b5e82b883
Morten <morten.boettger@netzkontor-nord.de>
2026-06-03 17:20:03 +0200

    fixed dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.1.21"
-    id("org.jetbrains.kotlin.plugin.allopen") version "2.1.21"
-    id("com.google.devtools.ksp") version "2.1.21-2.0.2"
+    id("org.jetbrains.kotlin.jvm") version "2.3.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "2.3.21"
+    id("com.google.devtools.ksp") version "2.3.9"
     id("com.gradleup.shadow") version "9.4.2"
     id("io.micronaut.application") version "5.0.0"
     id("io.micronaut.aot") version "5.0.0"
@@ -42,8 +42,8 @@ application {
     mainClass.set("io.mboettger.Application")
 }
 java {
-    sourceCompatibility = JavaVersion.toVersion("23")
-    targetCompatibility = JavaVersion.toVersion("23")
+    sourceCompatibility = JavaVersion.toVersion("25")
+    targetCompatibility = JavaVersion.toVersion("25")
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(25))
     }
@@ -52,12 +52,12 @@ java {
 tasks {
     compileKotlin {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_23)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_25)
         }
     }
     compileTestKotlin {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_23)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_25)
         }
     }
 
